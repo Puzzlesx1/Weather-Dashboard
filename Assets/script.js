6b994a51c552a3fbbe590fb5fc4ca9af27f87190
Christian Kay <Christiankay@rockconnections.com>
2022-09-22 20:38:06 -0400

    added text to html

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,45 @@
+//set variables//
 var button = document.getElementById("button");
 var city = document.getElementById("city");
+var tempInput = document.getElementById("temp");
+var windInput = document.getElementById("wind");
+var humidInput = document.getElementById("humid");
+var cityDate = document.getElementById("cityName");
+var searches = [];
 
 button.addEventListener('click',getWeather)
+
 
 function getWeather() {
 var cityName = city.value
 
-var url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=a60c04584e3194d68bf2d0b0e4139e9e`
+var url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=b4071a725cdccd56d8b70a432022d346`
 fetch(url).then(function(response){
     return response.json()
 }).then(function(data){
     console.log(data)
     var lon = data[0].lon
     var lat = data[0].lat
-    var url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=a60c04584e3194d68bf2d0b0e4139e9e`
+    var url2 = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b4071a725cdccd56d8b70a432022d346&units=imperial`
     fetch(url2).then(function(response){
         return response.json()
     }).then(function(data){
         console.log(data);
 
-        var temp = data.current.temp;
+        var name = data.name;
+        var date = data.dt;
+        var temp = data.main.temp;
+        var wind = data.wind.speed;
+
+
+
+        cityDate.innerHTML += name + " (" + dayjs(date * 1000).format("MM/DD/YYYY") + ") ".append(`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></img>`);
+        tempInput.innerHTML += temp +" °F";
+        windInput.innerHTML += wind +" mph";
+        humidInput.innerHTML += humidity +" %";
     
     })
 })
 }
+
+localStorage.setItem
